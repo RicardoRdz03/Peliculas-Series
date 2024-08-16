897771dcabfd60ad3f416b1bcc5d6de834d666b9
RicardoRdz03 <ricardo.arteaga.rdz@gmail.com>
2024-08-16 14:20:05 -0600

    Arreglando titulos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import InfoPeli from "./pages/InfoPeli";
 import PaginaPrincipal from "./pages/PaginaPrincipal";
 import Peliculas from "./pages/Peliculas";
 import Series from "./pages/Series";
 import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Layout({ titulo, children }) {
+  useEffect(() => {
+    document.title = titulo;
+  }, [titulo]);
+  return <div>{children}</div>;
+}
 
 function App() {
   return (
@@ -12,8 +20,22 @@ function App() {
         <Routes>
           <Route path="/" element={<PaginaPrincipal />} />
           <Route path="/pelicula/:titulo" element={<InfoPeli />} />
-          <Route path="/peliculas" element={<Peliculas />} />
-          <Route path="/series" element={<Series />} />
+          <Route
+            path="/peliculas"
+            element={
+              <Layout titulo="Peliculas">
+                <Peliculas />
+              </Layout>
+            }
+          />
+          <Route
+            path="/series"
+            element={
+              <Layout titulo="Series">
+                <Series />
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
     </>
